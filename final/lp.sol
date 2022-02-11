b54deeea12bece955fff5a47ee4ba92be4cba3e7
CrazyCaptian <dottage@gmail.com>
2022-02-11 04:19:33 -0600

    Update lp.sol

--- final/lp.sol
+++ final/lp.sol
@@ -1022,3 +1022,4 @@ function getRewardBasicBasic(uint choice) public updateReward(msg.sender) {
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 */
+//Staking Rewards
